8bb843c27f6663a449c9c99d63d44e00e1bf3feb
thayne <kikansenkyoshi@hotmail.com>
2016-12-01 22:09:05 -0700

    Add argparse. Use CBSCL.

--- Driver.py
+++ Driver.py
@@ -1,22 +1,48 @@
+#!/usr/bin/env python
+
 """
-Driver/Testing class for the airplane CBS
+Driver/Testing class for CBS
 David Chan - 2016
 """
 
 from CBS import CBS
+from CBSCL import CBSCL
 from AirplaneAgent import AirplaneAgent
-from AirplaneEnvironment import AirplaneEnvironment
+import argparse
+import random
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Solve MAPF Instance")
+    parser.add_argument("-n", "--num-agents", type=int, default=5, help="Number of agents to simulate. default=5")
+    parser.add_argument("-s", "--seed", type=int, default=None, help="Seed for random number generator. default=None")
+    parser.add_argument("-e", "--environments", default="AirplaneEnvironment", help="Comma-delimited list of environment names. default=AirplaneEnvironment")
+    parser.add_argument("-a", "--agent-type", default="AirplaneAgent", help="Agent type name. default=AirplaneAgent")
+    return parser.parse_args()
+
 
 def main():
     """ Main method """
-    # Generate 5 random agents
-    agents = [AirplaneAgent.random_agent() for _ in range(0, 2)]
+    # Parse arguments
+    args = parse_args()
 
-    # Make an Environment
-    airplane_env = AirplaneEnvironment()
+    if random.seed is not None:
+        random.seed(args.seed) 
 
+    exec("from %s import %s"%(args.agent_type,args.agent_type))
+    # Generate random agents
+    agents = eval("[%s.random_agent() for _ in range(0, args.num_agents)]"%args.agent_type)
+
+    env_names=args.environments.split(",")
+    
+    envs=[]
+    for e in env_names:
+        exec("from %s import %s"%(e,e))
+        # Make an Environment
+        envs.append(eval("%s()"%e))
+
+    print envs
     # Make a new CBS
-    main_cbs = CBS(airplane_env)
+    main_cbs = CBSCL(envs)
 
     # Add them to the CBS
     for agent in agents:
